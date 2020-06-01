646569f78c2150b4a3f29fe846a4277765463f87
cuimuhua <luckycmh@163.com>
2020-06-01 09:57:04 +0800

    [ add ]test space

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
+                
                 {this.props.children}
             </div>
         );
