ff1e92a12a3991800a047eaa45d5051f8df4febb
cuimuhua <luckycmh@163.com>
2020-10-20 10:29:07 +0800

    [ add ]顶部导航功能

--- src/components/topNav/index.js
+++ src/components/topNav/index.js
@@ -1,12 +1,13 @@
-import React,{useState,useEffect} from "react";
-import {useSelector,useDispatch} from "react-redux";
+import React, {useState, useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
 import {useHistory} from 'react-router-dom'
-import { Tabs } from 'antd';
-import {handleTopNav,handleActivePath} from "../../redux/common/actions";
+import {Tabs} from 'antd';
+import {handleTopNav, handleActivePath} from "../../redux/common/actions";
 import './index.less'
-const { TabPane } = Tabs;
 
-export default function(){
+const {TabPane} = Tabs;
+
+export default function () {
     //路由
     const history = useHistory();
     // 顶部导航数据
@@ -15,12 +16,31 @@ export default function(){
     const active = useSelector(state => state.activeTabPath);
     //派发函数
     const dispatch = useDispatch();
+    //记录删除的当前index
+    const [index, setIndex] = useState(-1);
     useEffect(() => {
-        console.log(tab);
-    },[tab]);
+        if (index !== -1) {
+            if (tab.length) {
+                if(index===0) {
+                    //从第一个开始删除
+                    history.push(tab[index].path);
+                } else {
+                    //倒序删除，寻找上一个path
+                    history.push(tab[index-1].path);
+                }
+
+            } else {
+                history.push('/admin/main');
+            }
+            //    重置初始化状态
+            setIndex(-1);
+        }
+    }, [index]);
     /*删除tab*/
-    const handleEdit = (targetKey,action) => {
-        dispatch(handleTopNav({type:'sub',tab:targetKey}));
+    const handleEdit = (targetKey, action) => {
+        const index = tab.findIndex(item => item.path === targetKey);
+        dispatch(handleTopNav({type: 'sub', tab: targetKey}));
+        setIndex(index);
     };
     //点击tab
     const handleChange = (activeKey) => {
